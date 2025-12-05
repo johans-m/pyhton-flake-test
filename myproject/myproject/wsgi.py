0b24cddd2d46ff50309059e2357450d6ba82f608
johans-m <johan@swapps.com>
2025-12-05 10:47:08 -0500

    test

--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -3,7 +3,7 @@ WSGI config for myproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this file, see 2123
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
