5d54c132e3a8562ebf1d039d8de94dcbc01eba14
johans-m <johan@swapps.com>
2025-12-05 12:56:17 -0500

    test

--- frontend/vue-project/src/App.vue
+++ frontend/vue-project/src/App.vue
@@ -2,12 +2,11 @@
 import HelloWorld from   './components/HelloWorld.vue'
 import TheWelcome from    './components/TheWelcome.vue'
 </script>
-
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
+    <div class="wrapper" >
       <HelloWorld msg="You did it!" />
     </div>
   </header>
@@ -19,20 +18,20 @@ import TheWelcome from    './components/TheWelcome.vue'
 
 <style scoped>
 header {
-  line-height: 1.5;
+  line-height: 1.5; 
 }
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  margin: 0 auto 2rem; 
 }
 
 @media (min-width: 1024px) {
-  header {
+  header { 
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
+   }
 
   .logo {
     margin: 0 2rem 0 0;
