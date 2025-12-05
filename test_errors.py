0da4a7b4f36043806e72d31e0373ec3d30a209fb
johans-m <johan@swapps.com>
2025-12-05 13:38:38 -0500

    fix python issues

--- test_errors.py
+++ test_errors.py
@@ -1,14 +1,17 @@
-# Este archivo tiene errores intencionales para probar el linter
-import os,sys,json
-x=1
-y=  2
-def bad_function( a,b,c ):
-    if x==1:
+# Archivo de prueba para el linter
+x = 1
+y = 2
+
+
+def bad_function(a, b, c):
+    if x == 1:
         print("mal formateado")
-    return a+b+c
-class badClass:
-    def __init__(self,name):
-        self.name=name
-    def get_name( self ):
+    return a + b + c
+
+
+class BadClass:
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
         return self.name
-unused_variable = "esto no se usa"
