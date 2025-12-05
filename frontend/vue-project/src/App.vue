6e0e98019e3d0ed7bf5281058a89d8f8b24effca
johans-m <johan@swapps.com>
2025-12-05 12:59:35 -0500

    test4

--- frontend/vue-project/src/App.vue
+++ frontend/vue-project/src/App.vue
@@ -1,18 +1,18 @@
 <script setup>
-import HelloWorld from   './components/HelloWorld.vue'
-import TheWelcome from    './components/TheWelcome.vue'
+import HelloWorld from   './components/HelloWorld.vue' 
+import TheWelcome from    './components/TheWelcome.vue' 
 </script>
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125"/>
 
     <div class="wrapper" >
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it!"/>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <TheWelcome/>
   </main>
 </template>
 
@@ -39,8 +39,8 @@ header {
 
   header .wrapper {
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    place-items: flex-start; 
+    flex-wrap: wrap; 
   }
 }
 </style>
