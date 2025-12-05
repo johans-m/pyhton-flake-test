647060676d201d1e8e780eea87bb7c7f379cd89d
johans-m <johan@swapps.com>
2025-12-05 10:40:34 -0500

    Test PR

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('', include('myapp.urls')), # this is a very long line that will cause a flake8 error because it is longer than 79 characters
 ]
